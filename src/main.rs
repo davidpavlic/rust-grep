6d92bba468e92516e4095d52269af1a5f222e625
David Pavlic <davidpavlic@MacBook-Pro-von-David.local>
2023-11-02 16:31:49 +0100

    Added simple args logic with prints

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::env;
+
 fn main() {
-    println!("Hello, rust-grep!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() > 1 {
+        match args[1].as_str() {
+            "hello" => { println!("Hello, rust-grep!"); },
+            "file" => if args.len() > 2 {
+                match args[2].as_str() {
+                    "r" => { println!("read file")},
+                    "c" => { println!("create file")}
+                    _ => { println!("Unknown Command")}
+                };
+            }else{
+                println!("<SECOND ARG> - r to read a file");
+                println!("             - c to create a file");
+            },
+            _ => { println!("Unknown Command...") }
+        }
+    }else{
+        println!("<FIRST ARG>  - hello for a greeting");
+        println!("             - file for file manipulation");
+    }
+    
 }
